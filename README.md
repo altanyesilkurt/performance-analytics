f09bddf6a9b0b8b69e17ed6432d839b92a4a16d1
Altan Yeşilkurt <altan.yesilkurt@litum.com>
2021-04-24 10:59:31 +0300

    added docker file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # performance-analytics
+  - I created a virtual machion on google cloud server and I create a  database on Postgres by using docker container.
+	(docker run --name [container_name] -e POSTGRES_PASSWORD=[your_password] -d postgres)
+  - I used react-svg-line-chart to show performance values.
+  - I write test code by using enzyme and enzyme-adapter-react-16 on client side. 
+  - I used window-performance built-in functions to mesaure performace issues.
+  - I used scss for styling especially for responsiveness on client side.
+  - I used express.js for api connection
+  - Demo page has <script src="https://performance-analytic.herokuapp.com/static/performance.js"></script> in index.html
+	I put in it for demonstrate the mesauring
+# How can I test the system ?
+
+  - Firstly, You should open https://legend-travel.netlify.app/  page which is dummy page for test
+  - Then, You should open https://perfanalytic-dashboard.netlify.app/  page which shows dashboard of the performance metrics
+  - You will see the chart take new point by refresh each page 
--- public/performance.js
+++ public/performance.js
@@ -1,37 +1,37 @@
-window.onload = function() {
-    // FCP
-    const paintEntries = window.performance.getEntriesByType("paint")[1];
-    const navigationEntries = performance.getEntriesByType("navigation")[0];
-    
-    console.log("paintEntries", paintEntries)
-    
-    //TTFB
-    const ttfb = navigationEntries.responseStart - navigationEntries.requestStart;
-    
-    // DOM lOAD (Check startTime)
-    const domLoad = navigationEntries.domComplete - navigationEntries.domContentLoadedEventStart;
-    
-    // WINDOW LOAD EVENTS
-    const windowLoadEvents = navigationEntries.loadEventStart - navigationEntries.loadEventEnd
-    
-    const siteName = window.location.href;
-    
-    const data = JSON.stringify({
-      site: siteName,
-      ttfb: ttfb,
-      fcp: paintEntries && paintEntries.startTime || 0,
-      domload: domLoad,
-      windowLoadEvents: windowLoadEvents
-      })
-      console.log("data",data)
-      fetch("http://localhost:3000", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: data
-      }).then(res => {
-        console.log("Request complete! response:", res);
-      });
-    
-    }
+window.onload = function () {
+  // FCP
+  const paintEntries = window.performance.getEntriesByType("paint")[1];
+  const navigationEntries = performance.getEntriesByType("navigation")[0];
+
+  //  const URL = "https://performance-analytic.herokuapp.com/"
+  const URL = "http://localhost:3000"
+
+  //TTFB
+  const ttfb = navigationEntries.responseStart - navigationEntries.requestStart;
+
+  // DOM lOAD 
+  const domLoad = navigationEntries.domComplete - navigationEntries.domContentLoadedEventStart;
+
+  // WINDOW LOAD 
+  const windowLoadEvents = navigationEntries.loadEventStart - navigationEntries.loadEventEnd
+
+  const siteName = window.location.href;
+
+  const data = JSON.stringify({
+    site: siteName,
+    ttfb: ttfb,
+    fcp: paintEntries && paintEntries.startTime || 0,
+    domload: domLoad,
+    windowLoadEvents: windowLoadEvents
+  })
+  fetch(URL, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: data
+  }).then(res => {
+    console.log("Fetch data request completed! response:", res);
+  });
+
+}
